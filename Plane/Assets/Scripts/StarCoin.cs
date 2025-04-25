ffeef0152cc5b5164312b602940b9e59b6735c5c
TakakusaNina <ninao0_0@icloud.com>
2025-04-25 13:58:57 +0900

    ワープ大修正とコインとゴール少し追記

--- Plane/Assets/Scripts/StarCoin.cs
+++ Plane/Assets/Scripts/StarCoin.cs
@@ -1,16 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
-using static UnityEngine.RuleTile.TilingRuleOutput;
-
-/*
-１ステージあたりコイン
-*/
+using static UnityEngine.GraphicsBuffer;
 
 public class StarCoin : MonoBehaviour
 {
     public bool isGetcoin = false;//Playerがコインに触れたらtrue
- 
+    private bool playerisexist = false;
+
     private void Start()
     {
 
@@ -20,12 +18,27 @@ public class StarCoin : MonoBehaviour
     {
     }
 
-    private void OnTriggerStay2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        Transform root = FindRootObject(other.transform);
+
+        if (other.CompareTag("PostIt") || other.CompareTag("Player"))
         {
-            isGetcoin = true;
-            gameObject.SetActive(false);
+            if (root.CompareTag("Player") || root.CompareTag("PostIt"))
+            {
+                isGetcoin = true;
+                gameObject.SetActive(false);
+            }
         }
+    }
+
+    private Transform FindRootObject(Transform obj)
+    {
+        Transform root = obj;
+        while (root.parent != null)
+        {
+            root = root.parent;
+        }
+        return root;
     }
 }
