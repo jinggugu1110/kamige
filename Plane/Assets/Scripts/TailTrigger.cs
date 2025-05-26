09dd97b51e39971db4ff68e3ba523e27d0b954ce
TakakusaNina <ninao0_0@icloud.com>
2025-05-26 10:10:50 +0900

    てき

---
+++ Plane/Assets/Scripts/TailTrigger.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TailTrigger : MonoBehaviour
+{
+    public global::Enemy enemy;
+    private bool movingRight = true;
+    private float wallCheckDistance = 0.6f;
+    private LayerMask wallLayer;
+    bool isHit = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("PostIt"))
+        {
+            Transform postItRoot = other.transform.root;
+            if (postItRoot.CompareTag("Player"))
+            {
+                return;
+            }
+
+            if (enemy != null)
+            {
+                enemy.OnTailHit();
+            }
+        }
+    }
+}
